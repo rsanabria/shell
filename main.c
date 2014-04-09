65376b6f65607ac0d27709043b9d0fd3283e197c
Edgar Mendoza <edgarmch@gmail.com>
2014-04-08 21:03:24 -0500

    ssh comentado

--- main.c
+++ main.c
@@ -6,52 +6,49 @@ char * token;
 char  prompt[6];
 char historial[100][20];
 int shellPid, fd;
-  int args;
+int args;
 
 main (){
   pthread_t history;
   char buffer[BUFFER_SIZE];
   strcpy(prompt,"#");
   prepararSenales();
-/*  int input = 0;
-  int first = 1;*/
-
+  
   while (1){
-	argumentos[0] = "\n"; //inicializar el arreglo para que no haya errores de punteros
-	argumentos[1] = "\n";
-	printf("[%s-%s] %s ", getlogin(), miPwd(), prompt);
-	fgets(buffer, BUFFER_SIZE, stdin);	
-	separa(buffer,argumentos);
-	pthread_create(&history, NULL, (void *)setHistorial, buffer);
-	if (!strcmp(argumentos[0],"salir")){
-	exit(0);
+    argumentos[0] = "\n"; //inicializar el arreglo para que no haya errores de punteros
+    argumentos[1] = "\n";
+    printf("[%s-%s] %s ", getlogin(), miPwd(), prompt);
+    fgets(buffer, BUFFER_SIZE, stdin);	
+    separa(buffer,argumentos);
+    pthread_create(&history, NULL, (void *)setHistorial, buffer);
+    if (!strcmp(argumentos[0],"salir")){
+      exit(0);
     }
-     else if (!strcmp(argumentos[0], "historial")){    
-                     mostrarHistorial();
-                  }      
+    else if (!strcmp(argumentos[0], "historial")){    
+      mostrarHistorial();
+    }      
     else if(!strcmp(argumentos[0],"cd")){
       chdir(argumentos[1]);
     }
-
-	else if (!strcmp(argumentos[0], "mipid")){
-		printf("Mi id de Proceso es %d\n",(int)getpid()); 
+    
+    else if (!strcmp(argumentos[0], "mipid")){
+      printf("Mi id de Proceso es %d\n",(int)getpid()); 
     }
     else {
       if(fork() == 0){
-		redireccion();	
-		if(execvp(argumentos[0], argumentos)<0 )
-		{
-			if (argumentos[0] == "\n"){}
-			else{
-			printf ("ese comando no existe\n");
-			}
-			exit(0);//matar al hijo
-		}
-
+	redireccion();	
+	if(execvp(argumentos[0], argumentos)<0 )
+	  {
+	    if (argumentos[0] == "\n"){}
+	    else{
+	      printf ("ese comando no existe\n");
+	    }
+	    exit(0);//matar al hijo
+	  }	
       }
       /* el padre espera al hijo */
-	pthread_join(history, NULL);
-       wait(NULL); 	
+      pthread_join(history, NULL);
+      wait(NULL); 	
     }
-}
+  }
 }
--- miShell.h
+++ miShell.h
@@ -8,220 +8,219 @@ extern int args;
 void separa( char *argumentos_buffer,char  *argumentos[]){
 /* Esta función recibe de argumento el buffer y va separar el buffer 
  * en subcadenas cada vez que encuente un espacio */
-	int i = 0;
-	char * token;
-	if (!strcmp(argumentos_buffer,"\n")){
-		}
-	else{
-	token = strtok(argumentos_buffer," \n");
-	while (token != NULL){
-		argumentos[i] = token;
-		i++;
-		token = strtok(NULL," \n");
-	}
-	argumentos[i] = NULL;
-   }
+  int i = 0;
+  char * token;
+  if (!strcmp(argumentos_buffer,"\n")){
+  }
+  else{
+    token = strtok(argumentos_buffer," \n");
+    while (token != NULL){
+      argumentos[i] = token;
+      i++;
+      token = strtok(NULL," \n");
+    }
+    argumentos[i] = NULL;
+  }
 }
 
 
 const char * miPwd(){
-/* Funcion que consigue el actual directorio y regresa la ultima carpeta
- * del path */         
-	char buffer[BUFFER_SIZE];
-	char *cwd;
-	char * dir;
-	char * token;
-	
-	cwd = getcwd(buffer, BUFFER_SIZE);
-	token = strtok( cwd, "/");
-	while (token != NULL){
-		dir = token;
-		token = strtok(NULL,"/");
-		}
-	return dir;
+  /* Funcion que consigue el actual directorio y regresa la ultima carpeta
+   * del path */         
+  char buffer[BUFFER_SIZE];
+  char *cwd;
+  char * dir;
+  char * token;
+  
+  cwd = getcwd(buffer, BUFFER_SIZE);
+  token = strtok( cwd, "/");
+  while (token != NULL){
+    dir = token;
+    token = strtok(NULL,"/");
+  }
+  return dir;
 }
 
-//Cambia el prompt dependiendo de señal recibida
+/*Cambia el prompt dependiendo de señal recibida*/
 void cambiarPrompt(int signal){	
-	switch(signal){
-		case 30:
-			strcpy(prompt,"%");
-			break;
-		case 31:
-			strcpy(prompt,":)");
-			break;
-		case 32:
-			strcpy(prompt,":0");
-			break;
-		default:
-			break;
-		}
+  switch(signal){
+  case 30:
+    strcpy(prompt,"%");
+    break;
+  case 31:
+    strcpy(prompt,":)");
+    break;
+  case 32:
+    strcpy(prompt,":0");
+    break;
+  default:
+    break;
+  }
 }
 
-//Prepara señales 30 a 32
+/*Prepara señales 30 a 32*/
 void prepararSenales(){
-	int i;
-	for(i=30;i<33;i++){
-		signal(i,cambiarPrompt);
-	
-	}
+  int i;
+  for(i=30;i<33;i++){
+    signal(i,cambiarPrompt);    
+  }
 }
+
+/*Funcion que prepara los redireccionamientos de archivos*/
 void redireccion(){
-	char *infile, *outfile, *outa, *outerr, *outamp;
-	char **cp;
-	int status;
-	infile = NULL;
-        outfile = NULL;
-	outa = NULL;
-	outerr = NULL;
-	outamp = NULL;
-
-        for (cp = argumentos; *cp != NULL; cp++){
-            if (strcmp(*cp, "<") == 0){
-                if (*(cp+1) == NULL){
-                    fprintf(stderr,"Debes especificar un archivo de entrada\n");
-			break;
-                }
-                *cp++ = NULL;
-                infile = *cp;
-            	}
-            else if (strcmp(*cp, ">") == 0){
-                if (*(cp+1) == NULL){
-                    fprintf(stderr, "Debes especificar un archivo de salida\n");
-	            break;
-                }
-                *cp++ = NULL;
-                outfile = *cp;
-            	}
-	  else if (strcmp(*cp, ">>") == 0){
-                if (*(cp+1) == NULL){
-                    fprintf(stderr, "Debes especificar un archivo de salida\n");
-	            break;
-                }
-                *cp++ = NULL;
-                outa = *cp;
-        }
-
-	else if (strcmp(*cp, "2>") == 0){
-                if (*(cp+1) == NULL){
-                    fprintf(stderr, "Debes especificar un archivo de salida\n");
-	            break;
-                }
-                *cp++ = NULL;
-                outerr = *cp;
-        }        
-
-	else if (strcmp(*cp, "&>") == 0){
-                if (*(cp+1) == NULL){
-                    fprintf(stderr, "Debes especificar un archivo de salida\n");
-	            break;
-                }
-                *cp++ = NULL;
-                outamp = *cp;
-            	}
-        }
-	status = execute(argumentos, infile, outfile, outa, outerr, outamp);
+  char *infile, *outfile, *outa, *outerr, *outamp;
+  char **cp;
+  int status;
+  infile = NULL;
+  outfile = NULL;
+  outa = NULL;
+  outerr = NULL;
+  outamp = NULL;
+  
+  for (cp = argumentos; *cp != NULL; cp++){
+    if (strcmp(*cp, "<") == 0){
+      if (*(cp+1) == NULL){
+	fprintf(stderr,"Debes especificar un archivo de entrada\n");
+	break;
+      }
+      *cp++ = NULL;
+      infile = *cp;
+    }
+    else if (strcmp(*cp, ">") == 0){
+      if (*(cp+1) == NULL){
+	fprintf(stderr, "Debes especificar un archivo de salida\n");
+	break;
+      }
+      *cp++ = NULL;
+      outfile = *cp;
+    }
+    else if (strcmp(*cp, ">>") == 0){
+      if (*(cp+1) == NULL){
+	fprintf(stderr, "Debes especificar un archivo de salida\n");
+	break;
+      }
+      *cp++ = NULL;
+      outa = *cp;
+    }    
+    else if (strcmp(*cp, "2>") == 0){
+      if (*(cp+1) == NULL){
+	fprintf(stderr, "Debes especificar un archivo de salida\n");
+	break;
+      }
+      *cp++ = NULL;
+      outerr = *cp;
+    }            
+    else if (strcmp(*cp, "&>") == 0){
+      if (*(cp+1) == NULL){
+	fprintf(stderr, "Debes especificar un archivo de salida\n");
+	break;
+      }
+      *cp++ = NULL;
+      outamp = *cp;
+    }
+  }
+  status = execute(argumentos, infile, outfile, outa, outerr, outamp);
 }
 
 
+/*Funcion que ejecuta los redireccionamientos de archivos*/
 int execute(char **argumentos, char *infile, char *outfile, char *outfilea, char *outfilerr, char *outfileamp)
 {
-    int status;
-    int in, out, outa, outerr, outamp;
-    in = -1;
-    out = -1;
-    outa = -1;
-    outerr = -1;
-    outamp = -1;
-    //Si un archivo de entrada fue recibido, abrirlo
-    if (infile != NULL) {
-        if ((in = open(infile, O_RDONLY)) < 0) {
-            perror(infile);
-            return(-1);
-        }
+  int status;
+  int in, out, outa, outerr, outamp;
+  in = -1;
+  out = -1;
+  outa = -1;
+  outerr = -1;
+  outamp = -1;
+  
+  //Si un archivo de entrada fue recibido, abrirlo
+  if (infile != NULL) {
+    if ((in = open(infile, O_RDONLY)) < 0) {
+      perror(infile);
+      return(-1);
     }
-
-    //Si un archivo de salida fue recibido, crearlo
-    if (outfile != NULL) {
-        if ((out = creat(outfile, 0666)) < 0) {
-            close(in);
-            return(-1);
-        }
+  }  
+  //Si un archivo de salida fue recibido, crearlo
+  if (outfile != NULL) {
+    if ((out = creat(outfile, 0666)) < 0) {
+      close(in);
+      return(-1);
     }
-
-    if (outfilea != NULL) {
-        if ((outa = open(outfilea, 0666)) < 0) {
-            close(in);
-            return(-1);
-        }
+  }  
+  if (outfilea != NULL) {
+    if ((outa = open(outfilea, 0666)) < 0) {
+      close(in);
+      return(-1);
     }
-
-    if (outfilerr != NULL) {
-        if ((outerr = creat(outfilerr, 0666)) < 0) {
-            close(in);
-            return(-1);
-        }
+  }
+  
+  if (outfilerr != NULL) {
+    if ((outerr = creat(outfilerr, 0666)) < 0) {
+      close(in);
+      return(-1);
     }
-
-    if (outfileamp != NULL) {
-        if ((outamp = creat(outfileamp, 0666)) < 0) {
-            close(in);
-            return(-1);
-        }
+  }  
+  if (outfileamp != NULL) {
+    if ((outamp = creat(outfileamp, 0666)) < 0) {
+      close(in);
+      return(-1);
     }
-   //Hacer redireccionamientos
-        if (in > 0){
-            dup2(in, 0);
-	}
-
-        if (out > 0)
-            dup2(out, 1);   
-
-	if(outa > 0)
-	      freopen(outfilea, "a+", stdout);  
-
-	if(outerr > 0)
-	      freopen(outfilerr, "w+", stderr);  
-
-	if(outamp > 0){
-	      freopen(outfileamp, "w+", stderr);  
-	      freopen(outfileamp, "w+", stdout);  
- 	}
-	// Cerrar los descriptores
-    close(out);
-    close(in);
-    close(outa);
-    close(outerr);
-    close(outamp);
- 
-    return(status);
+  }
+  //Hacer redireccionamientos
+  if (in > 0){
+    dup2(in, 0);
+  }
+  
+  if (out > 0)
+    dup2(out, 1);   
+  
+  if(outa > 0)
+    freopen(outfilea, "a+", stdout);  
+  
+  if(outerr > 0)
+    freopen(outfilerr, "w+", stderr);  
+  
+  if(outamp > 0){
+    freopen(outfileamp, "w+", stderr);  
+    freopen(outfileamp, "w+", stdout);  
+  }
+  // Cerrar los descriptores
+  close(out);
+  close(in);
+  close(outa);
+  close(outerr);
+  close(outamp);
+  
+  return(status);
 }
 
 //Despliega el historial en pantalla
- void mostrarHistorial(void)
+void mostrarHistorial(void)
+{
+  int i = 0;
+  while(historial[i][0])
     {
-        int i = 0;
-        while(historial[i][0])
-        {
-                printf("%d  %s\n", i+1, *(historial + i));
-                i++;
-        }
+      printf("%d  %s\n", i+1, *(historial + i));
+      i++;
+    }
 }
 
+/*Funcion que almacena el historial*/
 void setHistorial(char *arg)
 {
-	// Guardar el historial desde linea de comandos
-        int i = 0;
-	time_t tiempo;
-	time(&tiempo);
-	FILE *fp;
-	fp = fopen("/home/edgarmch/tmp/archivo.txt","w");
-	fprintf(fp, "%s\n", ctime(&tiempo) );//imprime la fecha y hora
-        while(historial[i][0])
-            {
-		fprintf(fp, "%s\n", *(historial + i) );//Imprime el historial a archivo oculto
-             	i++;
-            }
-        strcpy(historial[i], arg);
-	fclose(fp);
+  // Guardar el historial desde linea de comandos
+  int i = 0;
+  time_t tiempo;
+  time(&tiempo);
+  FILE *fp;
+  fp = fopen("/tmp/archivo.txt","w");
+  fprintf(fp, "%s\n", ctime(&tiempo) );//imprime la fecha y hora
+  while(historial[i][0])
+    {
+      fprintf(fp, "%s\n", *(historial + i) );//Imprime el historial a archivo oculto
+      i++;
+    }
+  strcpy(historial[i], arg);
+  fclose(fp);
 }
